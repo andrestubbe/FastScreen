db1eec5b8dd82b734bd1138529c3db17995c3544
Andre Stubbe <andrestubbe@proton.me>
2026-09-05 12:20:38 +0200

    Revert "feat: implement AutoCloseable in FastScreen"
    This reverts commit 69f516fdc42e4c73e7d24841d797f5292356bff9.

--- src/main/java/fastscreen/FastScreen.java
+++ src/main/java/fastscreen/FastScreen.java
@@ -43,7 +43,7 @@ import java.nio.ByteBuffer;
  * @version 1.0.0
  * @since 2026-04-16
  */
-public class FastScreen implements AutoCloseable {
+public class FastScreen {
 
     static {
         // Load native library via FastCore
@@ -319,14 +319,6 @@ public class FastScreen implements AutoCloseable {
             nativeDispose(nativeHandle);
             nativeHandle = 0;
         }
-    }
-
-    /**
-     * Closes this FastScreen instance, releasing all native GPU resources.
-     */
-    @Override
-    public void close() {
-        dispose();
     }
 
     /**
